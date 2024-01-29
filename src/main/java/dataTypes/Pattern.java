3d77703d177b8276323f4872323c10841944e86a
taskeenfatima <taskeen.fatima@movinx.com>
2024-01-29 16:13:16 +0100

    added pattern

---
+++ src/main/java/dataTypes/Pattern.java
@@ -0,0 +1,14 @@
+package dataTypes;
+
+public class Pattern {
+
+    public static void main(String[] args) {
+
+        int testInt = 12345;
+        String testString = "ABC";
+
+        String pattern = "%$s ******* + testString ******";
+        System.out.println(pattern);
+
+    }
+}
