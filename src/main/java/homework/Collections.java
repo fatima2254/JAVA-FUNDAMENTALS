cc2d39ab07a5b2c3548342252eab6833b7d7f5d0
Mihail_Damasheuski <mihail_damasheuski@epam.com>
2024-04-02 15:51:51 +0300

    Added homework

---
+++ src/main/java/homework/Collections.java
@@ -0,0 +1,58 @@
+public class Collections {
+
+    /**
+     * Exercise #1
+     *
+     * Create class Student with fileds:
+     * 1. Name
+     * 2. Grade
+     * 3. Age
+     * 4. Sport(Soccer, Hockey, Tennis, Baseball)
+     * 5. Team(Tigers, Ice Bulls, Angry Dogs)
+     * 6. Hobby(Guitar, Video games, Books)
+     *
+     * Create at least 20 Student objects and put them into a List collection
+     *
+     * Implement mothod that will take List<Student> currentStudents as input parameter and will print full information about student
+     * Implement method that will take List<Student> as input parameter and will print team and name of the student for each type of sport
+     * Implement method that will take List<Student> as input parameter and will print how many student like to play guitar in each team
+     */
+
+
+    /**
+     * Exercise #2
+     *
+     * Use List<Student> from previous exercise.
+     *
+     * 1. Remove from the collection all students who's hobby is `Video games`
+     * 2. Create a separate List<Students> newStudents and put 10 new students with hobby Cooking
+     * 3. Replace students from list <oldStudents> with students from list <newStudents> if their names are the same
+     *  e.g.    <oldStudents>: <Mike, 4, 17, Soccker, Tigers, Guitar> should be replaced by
+     *          <newStudents>: <Mike, 6, 15, Tennis, Ice Bulls, Books>
+     * 3. Add all not added previously students from <newStudents> into <oldStudents>
+     */
+
+    /**
+     * Exercise #3
+     *
+     * 1. Print number of unique words in this topic:
+     "You don’t get on by being original. You don’t get on by being bright. You don’t get on by being strong.
+     You get on by being a subtle crook; somehow he thinks that’s what Norris is, and he feels an irrational dislike
+     taking root, and he tries to dismiss it, because he prefers his dislikes rational, but after all, these
+     circumstances are extreme, the cardinal in the mud, the humiliating tussle to get him back in the saddle,
+     the talking, talking on the barge, and worse, the talking, talking on his knees, as if Wolsey’s unravelling,
+     in a great unweaving of scarlet thread that might lead you back into a scarlet labyrinth,
+     with a dying monster at its heart."
+     *
+     */
+
+
+    /**
+     * Exercise #4
+     * 1. Find unique words in this string:
+     " Number, madam, flag, car, smartphone, watch, baseball, horse, radar, Alan, wall, eye, window, jacket, dog, hat, female,
+     level, stone, ticket, water, keyboard, hand, car, hat, wall, stone, hair, rotator, time, table, car, dog, Hannah"
+     * 2. Find all repeated words. And how many times they are repeated
+     * 3. Find and print all the palindromes
+     */
+}
